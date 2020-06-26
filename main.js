83b9db29dc98d9eb0c0b08b102053ea11e2d1e33
5G7K <60315312+5G7K@users.noreply.github.com>
2020-06-26 08:20:02 +0200

    Fix only outside temperature
    end of sunprotection fix

--- main.js
+++ main.js
@@ -2759,7 +2759,7 @@ function sunProtect() {
                                                             let hysteresisOutside = (((100 - result[i].hysteresisOutside) / 100) * result[i].tempOutside).toFixed(2);
                                                             let hysteresisLight = (((100 - result[i].hysteresisLight) / 100) * result[i].valueLight).toFixed(2);
 
-                                                            if ((parseFloat(hysteresisOutside) > outsideTemp && result[i].lightSensor != '' && parseFloat(hysteresisLight) > sunLight) || (parseFloat(hysteresisOutside) > outsideTemp && result[i].lightSensor == '')) {
+                                                            if ((parseFloat(hysteresisOutside) > outsideTemp || result[i].lightSensor != '' && parseFloat(hysteresisLight) > sunLight) || (parseFloat(hysteresisOutside) > outsideTemp && result[i].lightSensor == '')) {
 
                                                                 /**
                                                                  * @param {any} err
