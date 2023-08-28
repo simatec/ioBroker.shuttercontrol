9fc480cbbcf522cb046bc20cf1c6ac7999cf2542
simatec <nais@gmx.net>
2023-08-28 11:59:13 +0200

    (simatec) Beta 1.6.2

--- main.js
+++ main.js
@@ -241,7 +241,7 @@ function startAdapter(options) {
                                 const nameDevice = shutterSettings[s].shutterName.replace(/[.;, ]/g, '_');
                                 const _shutterState = await adapter.getForeignStateAsync(shutterSettings[s].name).catch((e) => adapter.log.warn(e));
 
-								if (typeof _shutterState != undefined && _shutterState != null && shutterSettings[s].oldHeight != Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound) {
+                                if (typeof _shutterState != undefined && _shutterState != null && shutterSettings[s].oldHeight != Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound) {
                                     adapter.log.debug('Shutter state changed: ' + shutterSettings[s].shutterName + ' old value = ' + shutterSettings[s].oldHeight + ' new value = ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound);
                                 }
                                 if (typeof _shutterState != undefined && _shutterState != null && Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound != shutterSettings[s].currentHeight && Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound != shutterSettings[s].oldHeight) {
@@ -280,33 +280,33 @@ function startAdapter(options) {
                                         }
                                     } else {
                                         if (shutterSettings[s].firstCompleteUp != true && shutterSettings[s].currentAction != 'none' && shutterSettings[s].currentAction != 'OpenInSunProtect') {
-											shutterSettings[s].currentAction = 'Manu_Mode';
-											shutterSettings[s].triggerAction = 'Manu_Mode';
-											adapter.log.debug('set Manu_Mode #1 ' + shutterSettings[s].shutterName); 
-										}
-										else {
-											//Shutter is closed -> open manually to heightUp (should be 100% or 0%) before it has been opened automatically -> enable possibility to activate sunprotect height if required --> if sunprotect is required: shutter is set to sunProtect height
-											if (shutterSettings[s].firstCompleteUp == true && Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound == shutterSettings[s].heightUp && shutterSettings[s].currentAction != 'up' && shutterSettings[s].currentAction != 'triggered' && shutterSettings[s].currentAction != 'triggered_Tilted') {
-												shutterSettings[s].currentHeight = Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound;
-												shutterSettings[s].triggerHeight = Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound;
-												shutterSettings[s].currentAction = 'none'; //reset mode. e.g. mode can be set to sunProtect later if window is closed
-												shutterSettings[s].triggerAction = 'Manu_Mode';
-												shutterSettings[s].firstCompleteUp = false;
-												adapter.log.debug('Reset firstCompleteUp #1 for ' + shutterSettings[s].shutterName);
-												adapter.log.debug(shutterSettings[s].shutterName + ' opened manually to ' + shutterSettings[s].heightUp + '. Old value = ' + shutterSettings[s].oldHeight + '. New value = ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound + '. Possibility to activate sunprotect enabled.');
+                                            shutterSettings[s].currentAction = 'Manu_Mode';
+                                            shutterSettings[s].triggerAction = 'Manu_Mode';
+                                            adapter.log.debug('set Manu_Mode #1 ' + shutterSettings[s].shutterName);
+                                        }
+                                        else {
+                                            //Shutter is closed -> open manually to heightUp (should be 100% or 0%) before it has been opened automatically -> enable possibility to activate sunprotect height if required --> if sunprotect is required: shutter is set to sunProtect height
+                                            if (shutterSettings[s].firstCompleteUp == true && Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound == shutterSettings[s].heightUp && shutterSettings[s].currentAction != 'up' && shutterSettings[s].currentAction != 'triggered' && shutterSettings[s].currentAction != 'triggered_Tilted') {
+                                                shutterSettings[s].currentHeight = Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound;
+                                                shutterSettings[s].triggerHeight = Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound;
+                                                shutterSettings[s].currentAction = 'none'; //reset mode. e.g. mode can be set to sunProtect later if window is closed
+                                                shutterSettings[s].triggerAction = 'Manu_Mode';
+                                                shutterSettings[s].firstCompleteUp = false;
+                                                adapter.log.debug('Reset firstCompleteUp #1 for ' + shutterSettings[s].shutterName);
+                                                adapter.log.debug(shutterSettings[s].shutterName + ' opened manually to ' + shutterSettings[s].heightUp + '. Old value = ' + shutterSettings[s].oldHeight + '. New value = ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound + '. Possibility to activate sunprotect enabled.');
 
-												await adapter.setStateAsync('shutters.autoState.' + nameDevice, { val: shutterSettings[s].currentAction, ack: true })
-													.catch((e) => adapter.log.warn(e));
-												await adapter.setStateAsync('shutters.autoLevel.' + nameDevice, { val: parseFloat(shutterSettings[s].currentHeight), ack: true })
-													.catch((e) => adapter.log.warn(e));
+                                                await adapter.setStateAsync('shutters.autoState.' + nameDevice, { val: shutterSettings[s].currentAction, ack: true })
+                                                    .catch((e) => adapter.log.warn(e));
+                                                await adapter.setStateAsync('shutters.autoLevel.' + nameDevice, { val: parseFloat(shutterSettings[s].currentHeight), ack: true })
+                                                    .catch((e) => adapter.log.warn(e));
 
-											}
-											else {
-												shutterSettings[s].currentAction = 'Manu_Mode';
-												shutterSettings[s].triggerAction = 'Manu_Mode';
-												adapter.log.debug('set Manu_Mode #2 ' + shutterSettings[s].shutterName); 
-											}										
-										}
+                                            }
+                                            else {
+                                                shutterSettings[s].currentAction = 'Manu_Mode';
+                                                shutterSettings[s].triggerAction = 'Manu_Mode';
+                                                adapter.log.debug('set Manu_Mode #2 ' + shutterSettings[s].shutterName);
+                                            }
+                                        }
 
                                         adapter.log.debug(shutterSettings[s].shutterName + ' drived manually to ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound + '. Old value = ' + shutterSettings[s].oldHeight + '. New value = ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound);
                                         adapter.log.debug(shutterSettings[s].shutterName + ' Updated trigger action to ' + shutterSettings[s].triggerAction + ' to prevent moving after window close ');
@@ -324,26 +324,28 @@ function startAdapter(options) {
                                     await adapter.setStateAsync('shutters.autoState.' + nameDevice, { val: shutterSettings[s].currentAction, ack: true })
                                         .catch((e) => adapter.log.warn(e));
 
-									await sleep(2000);
+                                    await sleep(2000);
                                     shutterSettings = await shutterState(shutterSettings[s].name, adapter, shutterSettings, false);
                                 } else if (typeof _shutterState != undefined && _shutterState != null && Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound === shutterSettings[s].currentHeight) {
                                     adapter.log.debug(shutterSettings[s].shutterName + ' Old value = ' + shutterSettings[s].oldHeight + '. New value = ' + Math.round(_shutterState.val / adapter.config.shutterStateRound) * adapter.config.shutterStateRound + '. automatic is active');
-									await sleep(2000);
-									shutterSettings = await shutterState(shutterSettings[s].name, adapter, shutterSettings, false);
+                                    await sleep(2000);
+                                    shutterSettings = await shutterState(shutterSettings[s].name, adapter, shutterSettings, false);
                                 }
 
                                 //save old height
                                 await sleep(2000); //already waited waitTime4StateCheck seconds.
-								var timeout_set_oldHeight;
-								timeout_set_oldHeight = setTimeout(async () => {	//async set with timeout because e.g. Shelly submits several position by MQTT and not only the last one (0% 1% ... 97% - 98% - 100%). By this the value is set only after the final height state has been processed
-								  timeout_set_oldHeight = null;
-								  shutterSettings[s].oldHeight = shutterSettings[s].currentHeight;
-								  if (shutterSettings[s].firstCompleteUp === true) {
-									  shutterSettings[s].firstCompleteUp = false;
-									  adapter.log.debug('Reset firstCompleteUp #2 for ' + shutterSettings[s].shutterName);
-								  }
-								}, 5000 + waitTime4StateCheck);
-								saveCurrentStates(false);
+
+                                let timeout_set_oldHeight;
+
+                                timeout_set_oldHeight = setTimeout(async () => {	//async set with timeout because e.g. Shelly submits several position by MQTT and not only the last one (0% 1% ... 97% - 98% - 100%). By this the value is set only after the final height state has been processed
+                                    timeout_set_oldHeight = null;
+                                    shutterSettings[s].oldHeight = shutterSettings[s].currentHeight;
+                                    if (shutterSettings[s].firstCompleteUp === true) {
+                                        shutterSettings[s].firstCompleteUp = false;
+                                        adapter.log.debug('Reset firstCompleteUp #2 for ' + shutterSettings[s].shutterName);
+                                    }
+                                }, 5000 + waitTime4StateCheck);
+                                saveCurrentStates(false);
                             }
                         }
                     }
@@ -726,7 +728,7 @@ const calc = schedule.scheduleJob('calcTimer', '30 2 * * *', async function () {
                         adapter.log.debug(resultStates[i].shutterName + ' level conversion is enabled');
                     }
                 }
-				saveCurrentStates(false);
+                saveCurrentStates(false);
             }
         }
     }
